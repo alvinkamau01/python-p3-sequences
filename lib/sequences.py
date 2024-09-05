8e72d2daed9b750bf4733fdaafc06bcbf03f3e18
Alvin Kamau <alvin.kamau@student.moringaschool.com>
2024-09-05 12:58:53 +0300

    .

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        print([])
+        return
+    elif length == 1:
+        print([0])
+        return
+    elif length == 2:
+        print([0, 1])
+        return
+
+    sequence = [0, 1]
+    while len(sequence) < length:
+        sequence.append(sequence[-1] + sequence[-2])
+    print(sequence)
